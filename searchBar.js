487bb69f26963b2559764584b1197c44f84793ee
mariapaulav <mariapaulaviviz@gmail.com>
2022-08-05 18:35:36 -0500

    search bar

---
+++ searchBar.js
@@ -0,0 +1,12 @@
+function theFunction(){
+    console.log('hi')
+}
+
+const btn = document.getElementById('inputName');
+
+btn.addEventListener('click', function handleClick(e) {
+  e.preventDefault();
+  const firstNameInput = document.getElementById('inputName');
+  console.log(firstNameInput.value);
+  firstNameInput.value = '';
+});
